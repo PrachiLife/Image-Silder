6680a8fc36c953ef41de2dc6e81f3a4df6b0085e
PrachiLife <prachi23january@gmail.com>
2024-02-22 15:38:46 +0530

    Initialize the Image Silder project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import {defineComponent} from 'vue'
+import ImageSilder from './components/ImageSlider.vue'
+export default defineComponent({
+  name:"App",
+  components:{
+    ImageSilder,
+  }
+})
+</script>
+
+<template>
+  <div>
+   <image-silder/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ImageSlider.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+    Image Silder
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name:"ImageSilder",
+})
+</script>
+
+<style scoped>
+
+</style>
